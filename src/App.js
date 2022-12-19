6be396588aa8c8ace4b1d61a7f2d6d0bc59e0978
askarsheripbaev <sheripbaev1221@gmail.com>
2022-12-20 00:05:39 +0600

    added react files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import TitleMain from "./components/main";
+import HeaderTitle from "./components";
+import FooterTitle from "./components/footer";
+
+function App() {
+  return (
+    <div className="container">
+<HeaderTitle />    
+<TitleMain />
+<FooterTitle />
+
+
+    </div>
+  );
+}
+
+export default App;
